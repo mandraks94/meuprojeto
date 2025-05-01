542fccc8d0dcbacd17e950d426f16be52489efbf
mandraks94 <je-md3@hotmail.com>
2025-05-01 00:25:07 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,13 +1,23 @@
-(function() {
-    if (location.href.includes("instagram.com")) {
-        function injectNaoSegueMenu() {
-            let div = document.createElement("div");
-            div.id = "naoSegueDiv";
-            div.innerHTML = "<h3>Não Seguem de Volta</h3><table><thead><tr><th>ID</th><th>Username</th><th><input type='checkbox' id='masterCheck'></th></tr></thead><tbody></tbody></table><br><button class='naoSegueBtn' onclick='document.querySelectorAll(`.userCheck`).forEach(c=>c.checked=true)'>Selecionar Tudo</button><button class='naoSegueBtn' onclick='document.querySelectorAll(`.userCheck`).forEach(c=>c.checked=false)'>Desmarcar Tudo</button><button class='naoSegueBtn' onclick='alert(`Função de unfollow em desenvolvimento`)'>Unfollow</button>";
-            document.body.appendChild(div);
-        }
+javascript:(function(){
+    if(window.location.href.includes("instagram.com")){
+        // Redirecionar para a página de Curtidas
+        document.getElementById('curtidasBtn').addEventListener('click', function(){
+            window.location.href = 'https://www.instagram.com/your_activity/interactions/likes/';
+        });
 
-        // Aqui seria o código que manipula o arquivo CSV de seguidores e seguindo, mas esse código só está estruturado para injetar a interface
-        injectNaoSegueMenu();
+        // Redirecionar para a página de Comentários
+        document.getElementById('comentariosBtn').addEventListener('click', function(){
+            window.location.href = 'https://www.instagram.com/your_activity/interactions/comments/';
+        });
+
+        // Redirecionar para a página de Bloqueados
+        document.getElementById('bloqueadosBtn').addEventListener('click', function(){
+            window.location.href = 'https://www.instagram.com/accounts/access_tool/block_list/';
+        });
+
+        // Redirecionar para a página de Direct
+        document.getElementById('mensagensBtn').addEventListener('click', function(){
+            window.location.href = 'https://www.instagram.com/direct/inbox/';
+        });
     }
 })();
